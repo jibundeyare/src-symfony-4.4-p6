6d4ff3dd09364338818c18a1543ee327348cc801
Jibundeyare <contact@jibundeyare.com>
2020-07-23 09:32:32 +0200

    Création d'un contrôleur pour la home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_index")
+     */
+    public function index()
+    {
+        return $this->redirectToRoute('post_index');
+    }
+}
